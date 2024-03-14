bcb2ed4b270bb2297fffd9bae63066b242dd0ca6
Ruselle Daanoy <133234541+RuselleDaanoy@users.noreply.github.com>
2024-03-15 00:14:06 +0800

    welcome finale

--- Welcome.java
+++ Welcome.java
@@ -18,11 +18,11 @@ public class Welcome extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         mainPanel = new JPanel(new BorderLayout());
-        mainPanel.setBackground(new Color(0, 0, 0));
+        mainPanel.setBackground(new Color(61, 96, 68));
         mainPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
 
         textPanel = new JPanel(new GridBagLayout());
-        Color panelColor = new Color(217, 217, 217);
+        Color panelColor = new Color(243, 234, 214);
         textPanel.setBackground(panelColor);
 
         gbc = new GridBagConstraints();
@@ -34,13 +34,13 @@ public class Welcome extends JFrame {
         hiLabel = new JLabel("Hi! are you new here?");
         hiLabel.setForeground(new Color(0, 0, 0));
         hiLabel.setFont(hiLabel.getFont().deriveFont(60f)); 
-        textPanel.add(hiLabel, createGbc());
+        textPanel.add(hiLabel, createGbc()); 
         textPanel.add(Box.createVerticalStrut(20), createGbc());
 
         gbc.gridy = 1;
         nameField = new JTextField(20);
         nameField.addActionListener(e -> showWelcomeMessage(nameField.getText())); 
-        textPanel.add(nameField, createGbc());
+        textPanel.add(nameField, createGbc()); 
         textPanel.add(Box.createVerticalStrut(20), createGbc());
 
         mainPanel.add(textPanel, BorderLayout.CENTER);
@@ -62,28 +62,28 @@ public class Welcome extends JFrame {
         }
         String capitalizedString = capitalized.toString().trim();
     
-        textPanel.removeAll(); 
-        hiLabel.setText("Welcome, " + capitalizedString + "!"); 
+        textPanel.removeAll();
+        hiLabel.setText("Welcome, " + capitalizedString + "!");
         textPanel.add(hiLabel, createGbc());
         textPanel.add(Box.createVerticalStrut(20), createGbc());
-    
+
         JPanel descriptionPanel = new JPanel(new BorderLayout());
-        descriptionPanel.setOpaque(false); 
+        descriptionPanel.setOpaque(false);
         JLabel descriptionLabel = new JLabel("<html><div style='text-align:center; width:600px; color:rgb(0,0,0); font-style: italic;'>We are TechTetra, and this is AlgoAerie.<br><br>It is an innovative and dynamic interactive platform, offering a visual journey through intricate algorithmic challenges and their tailored solutions. Seamlessly blending user-friendly graphics with in-depth problem exploration, AlgoAerie illuminates the intricate tapestry of algorithmic problem-solving like never before.</div></html>");
         descriptionLabel.setFont(descriptionLabel.getFont().deriveFont(18f)); 
         descriptionPanel.add(descriptionLabel, BorderLayout.CENTER);
-        textPanel.add(descriptionPanel, createGbc()); 
-        textPanel.add(Box.createVerticalStrut(20), createGbc());
-    
-        JPanel additionalTextPanel = new JPanel(new GridBagLayout());
-        additionalTextPanel.setOpaque(false); 
-        JLabel additionalTextLabel = new JLabel("<html><span style='background-color: #000000; font-style: italic;'>Are you ready to unveil algorithms and craft solutions?</span></html>");
-        additionalTextLabel.setForeground(new Color(255, 255, 255));
-        additionalTextLabel.setFont(additionalTextLabel.getFont().deriveFont(18f));
-        additionalTextPanel.add(additionalTextLabel, createGbc());
-        textPanel.add(additionalTextPanel, createGbc());
+        textPanel.add(descriptionPanel, createGbc());
         textPanel.add(Box.createVerticalStrut(20), createGbc());
 
+        JPanel additionalTextPanel = new JPanel(new GridBagLayout());
+        additionalTextPanel.setOpaque(false); 
+        JLabel additionalTextLabel = new JLabel("<html><span style='background-color: #70823E; font-style: italic;'>Are you ready to unveil algorithms and craft solutions?</span></html>");
+        additionalTextLabel.setForeground(new Color(255, 255, 255));
+        additionalTextLabel.setFont(additionalTextLabel.getFont().deriveFont(18f)); 
+        additionalTextPanel.add(additionalTextLabel, createGbc()); 
+        textPanel.add(additionalTextPanel, createGbc()); 
+        textPanel.add(Box.createVerticalStrut(20), createGbc());
+    
         RoundedButtonPanel enterButton = new RoundedButtonPanel("I am!");
 
         enterButton.addActionListener(e -> {
@@ -98,21 +98,20 @@ public class Welcome extends JFrame {
 
     
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        bottomPanel.setBackground(new Color(217, 217, 217));
+        bottomPanel.setBackground(new Color(243, 234, 214));
         bottomPanel.add(enterButton);
     
-        textPanel.add(bottomPanel, createGbc());
+        textPanel.add(bottomPanel, createGbc()); 
     
         mainPanel.revalidate(); 
-    }
-    
+    }    
 
     private GridBagConstraints createGbc() {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.weightx = 1;
         gbc.anchor = GridBagConstraints.CENTER;
-        gbc.insets = new Insets(5, 0, 5, 0);
+        gbc.insets = new Insets(5, 0, 5, 0); 
         return gbc;
     }
 
