bb5b642cc806fe88f0cce2117afabe92860a1882
Ruselle Daanoy <133234541+RuselleDaanoy@users.noreply.github.com>
2024-03-14 22:04:43 +0800

    color changes nalang

--- DisplayPane.java
+++ DisplayPane.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 public class DisplayPane extends JFrame {
 
     private JPanel outputPanel;
-    private KnapsackNavigatorUI knapsackUI; 
+    private KnapsackNavigatorUI knapsackUI;
 
     public DisplayPane() {
         initComponents();
@@ -24,17 +24,19 @@ public class DisplayPane extends JFrame {
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(new Color(10, 20, 30));
+        mainPanel.setBackground(new Color(0, 0, 0));
         setContentPane(mainPanel);
-
-        JLabel problemOutputLabel = new JLabel("Problem Output");
+    
+        JLabel problemOutputLabel = new JLabel("Crafting Pane");
         problemOutputLabel.setFont(new Font("Arial", Font.BOLD, 20));
         problemOutputLabel.setForeground(Color.WHITE);
         problemOutputLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        problemOutputLabel.setPreferredSize(new Dimension(1280, 50));
         mainPanel.add(problemOutputLabel, BorderLayout.NORTH);
 
         outputPanel = new JPanel();
-        outputPanel.setBackground(new Color(255, 255, 225));
+        outputPanel.setBackground(new Color(217, 217, 217));
         outputPanel.setLayout(new BorderLayout());
         mainPanel.add(outputPanel, BorderLayout.CENTER);
 
@@ -45,11 +47,20 @@ public class DisplayPane extends JFrame {
 
     private JPanel createSideBar(String[] values) {
         JPanel sideBarPanel = new JPanel();
-        sideBarPanel.setLayout(new BoxLayout(sideBarPanel, BoxLayout.Y_AXIS));
-        sideBarPanel.setBackground(new Color(50, 50, 50));
-
-        for (String value : values) {
+        sideBarPanel.setLayout(null); 
+        sideBarPanel.setBackground(new Color(84, 84, 84));
+    
+        int buttonWidth = 260; 
+        int buttonHeight = 100; 
+        int verticalGap = 20; 
+        int startY = 25; 
+    
+        for (int i = 0; i < values.length; i++) {
+            String value = values[i];
             RoundedButtonPanel buttonPanel = new RoundedButtonPanel(value);
+            int panelX = 20; 
+            int panelY = startY + i * (buttonHeight + verticalGap); 
+            buttonPanel.setBounds(panelX, panelY, buttonWidth, buttonHeight);
             buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
             buttonPanel.addActionListener(new ActionListener() {
                 @Override
@@ -58,68 +69,55 @@ public class DisplayPane extends JFrame {
                 }
             });
             sideBarPanel.add(buttonPanel);
-            sideBarPanel.add(Box.createVerticalStrut(10)); 
         }
-
+    
+        int preferredHeight = startY + values.length * buttonHeight + (values.length - 1) * verticalGap;
+        sideBarPanel.setPreferredSize(new Dimension(buttonWidth + 40, preferredHeight)); 
         return sideBarPanel;
     }
 
     private void handleButtonClick(String buttonText) {
         outputPanel.removeAll();
         if (buttonText.equals("Knapsack")) {
-            showInformationMessage();
+            knapsackUI = new KnapsackNavigatorUI(); 
+            outputPanel.add(knapsackUI.getMainPanel(), BorderLayout.CENTER);
         } else if (buttonText.equals("EXIT!!!")) {
-            JOptionPane.showMessageDialog(this, "Exiting...");
-            System.exit(0);
+            String message = "<html><body style='font-family: Arial, sans-serif; font-size: 10px;'>";
+            message += "<div style='border: 1px solid #ccc; padding: 5px;'>";
+            message += "<i>As you depart, introducing the visionary minds behind AlgoaArie's innovative GUI, TechTetra proudly presents:</i><br/>";
+            message += "<ul style='list-style-type: none; padding-left: 0; margin-top: 3px;'>";
+            message += "<br><li><i>Faye Camille Buri</i></li>";
+            message += "<li><i>Louise Andrea Tatoy</i></li>";
+            message += "<li><i>Ruselle Daanoy</i></li>";
+            message += "<li><i>Royal Jones Go</i></li>";
+            message += "</ul>";
+            message += "</div>";
+            message += "</body></html>";
+
+            JDialog dialog = new JDialog();
+            dialog.setTitle("Good bye!");
+            JLabel label = new JLabel(message);
+            dialog.add(label);
+
+            JButton goodbyeButton = new JButton("Good Bye!");
+            goodbyeButton.setMaximumSize(new Dimension(100, 30)); 
+            goodbyeButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    dialog.dispose(); 
+                    System.exit(0);
+                }
+            });
+            
+            dialog.add(goodbyeButton, BorderLayout.SOUTH);
+
+            dialog.setSize(500, 200);
+            dialog.setLocationRelativeTo(this); 
+            dialog.setVisible(true);
         } else {
             outputPanel.add(new JLabel("Output for: " + buttonText), BorderLayout.CENTER);
-            outputPanel.revalidate();
-            outputPanel.repaint();
         }
-    }
-
-    private void showInformationMessage() {
-        String message = "<html><div style='text-align: center;'><font face='Tahoma' size='20' color='black'><b>KNAPSACK:</b></font><br>" +
-                "Identify the compatible cargo for the vehicle, adhering to strict weight restrictions. The vehicle can accommodate loads ranging from a minimum of 1 kilogram to a maximum of 15 kilograms.<br>" +
-                "<br>" +
-                "<table border='1'>" +
-                "<tr><td>Product Name</td><td>Weight per Unit (kg)</td><td>Quantity Available</td></tr>" +
-                "<tr><td>Canned Goods</td><td>5</td><td>450</td></tr>" +
-                "<tr><td>Cooking Oil</td><td>3</td><td>725</td></tr>" +
-                "<tr><td>Noodles</td><td>2.5</td><td>375</td></tr>" +
-                "<tr><td>Soap</td><td>7</td><td>500</td></tr>" +
-                "</table></div></html>";
-    
-        Rectangle outputBounds = outputPanel.getBounds();
-    
-        JDialog dialog = new JDialog(this, "Information", true);
-        dialog.setSize(980, 665);
-    
-        int dialogX = outputBounds.x + 320; 
-        int dialogY = outputBounds.y + 210; 
-        dialog.setLocation(dialogX, dialogY);
-    
-        JLabel messageLabel = new JLabel(message);
-        messageLabel.setBounds(10, 10, 960, 500);
-    
-        RoundedButtonPanel proceedButton = new RoundedButtonPanel("Proceed");
-        proceedButton.setBounds(410, 520, 160, 40); 
-        proceedButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose(); 
-                outputPanel.removeAll();
-                outputPanel.add(knapsackUI.getMainPanel(), BorderLayout.CENTER);
-                outputPanel.revalidate();
-                outputPanel.repaint();
-            }
-        });
-    
-        dialog.setLayout(null); 
-        dialog.add(messageLabel);
-        dialog.add(proceedButton);
-
-        dialog.setVisible(true);
+        outputPanel.revalidate();
+        outputPanel.repaint();
     }
 
     public static void main(String[] args) {
