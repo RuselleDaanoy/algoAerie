78f66fe06006e14a84a48d329e5615111315574c
Ruselle Daanoy <133234541+RuselleDaanoy@users.noreply.github.com>
2024-03-15 21:39:00 +0800

    finalProd

--- Product.java
+++ Product.java
@@ -10,4 +10,9 @@ public class Product {
         this.weight = weight;
         this.amount = amount;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s%-10s%-10s", name, weight, amount);
+    }
 }
